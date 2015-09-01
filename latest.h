fb155ed61f60df25baefccb0f97f0f7dbdb7fb63
ProDota2 <igorzv013@gmail.com>
2015-09-01 15:08:11 +0300

    01.09.2015 15:08

--- latest.h
+++ latest.h
@@ -2,13 +2,15 @@
 #include <vector>
 #include <queue>
 #include <string>
+#include <random>
+#include <cstdlib>
 using namespace std;
-const __int64 INF = 1LL<<57;
+const __int64 INF = 1LL << 57;
 
 /*
 Алгоритм Крускала
 */
-
+__int64 p[1000000];
 __int64 find(__int64 x){
 	return (p[x] == x ? x : p[x] = find(p[x]));
 }
@@ -17,12 +19,6 @@ void Union(__int64 v1, __int64 v2){
 }
 struct edge{
 	__int64 v, w, cost;
-	void r(){
-		cin >> v >> w >> cost;
-	}
-	void write(){
-		cout << v << ' ' << w << ' ' << cost << endl;
-	}
 };
 edge make_edge(__int64 a, __int64 b, __int64 c){
 	edge new_edge;
@@ -36,31 +32,8 @@ bool edge_sort(edge a, edge b){
 		return true;
 	return false;
 }
-//Example
-//Выводит минимальное остовное дерево 
-cin >> x >> y;
-for (__int64 i = 0; i < x;i++)
-	p[i] = i;
-for (__int64 i = 0; i < y;i++){
-	__int64 a, b, c;
-	cin >> a >> b >> c;
-	g.push_back(make_edge(a, b, c));
-	g.push_back(meke_edge(b, a, c));
-}
-__int64 ans = 0;
-sort(g.begin(), g.end(), edge_sort);
-for (__int64 i = 0i < g.size();i++){
-	__int64 v, w;
-	v = g[i].v;
-	w = g[i].w;
-	if (find(v) != find(w)){
-		Union(v, w);
-		ans += g[i].cost;
-	}
-}
-cout << ans;
 /*
-Декартово дерево 
+Декартово дерево
 merge - обьединяет два дерева
 split - разделяет на два дерева по Х
 del = delete
@@ -72,125 +45,131 @@ struct node{
 	node *l, *r;
 };
 node *root;
-node * merge(node *l,node *r){
-	if(l==NULL)
+node * merge(node *l, node *r){
+	if (l == NULL)
 		return r;
-	if(r==NULL)
+	if (r == NULL)
 		return l;
-	if(l->y > r->y){
-		l->r=merge(l->r,r);
+	if (l->y > r->y){
+		l->r = merge(l->r, r);
 		return l;
-	}else{
-		r->l=merge(l,r->l);
+	}
+	else{
+		r->l = merge(l, r->l);
 		return r;
 	}
 }
-void split(node *v,__int64 x,node *&l,node *&r){
-	if(v==NULL){
-		l=r=NULL;
+void split(node *v, __int64 x, node *&l, node *&r){
+	if (v == NULL){
+		l = r = NULL;
 		return;
 	}
-	if(v->x<=x){
-		split(v->r,x,v->r,r);
-		l=v;
-	}else{
-		split(v->l,x,l,v->l);
-		r=v;
+	if (v->x <= x){
+		split(v->r, x, v->r, r);
+		l = v;
+	}
+	else{
+		split(v->l, x, l, v->l);
+		r = v;
 	}
 }
 void del(__int64 x){
-	node *l,*m,*r;
-	split(root,x-1,l,m);
-	split(m,x,m,r);
-	root=merge(l,r);
+	node *l, *m, *r;
+	split(root, x - 1, l, m);
+	split(m, x, m, r);
+	root = merge(l, r);
 }
-bool find(__int64 x,node *r){
+bool find(__int64 x, node *r){
 	if (r == NULL)
 		return false;
-	if(r->x==x)
-		return true;	
+	if (r->x == x)
+		return true;
 	if (r->x < x){
 		return find(x, r->r);
-	}else{
+	}
+	else{
 		return find(x, r->l);
 	}
 }
 void insert(__int64 x){
 	node *l, *r;
-	node *v=new node;
+	node *v = new node;
 	v->x = x;
 	v->y = rand();
 	v->l = NULL;
 	v->r = NULL;
 	split(root, x, l, r);
-	root = merge(merge(l, v),r);
+	root = merge(merge(l, v), r);
 }
 
 /*
 Если prime[i] = false то i простое число
 */
-bool prime[10000000];
-prime[0] = prime[1] = true;
-for (__int64 i=2; i<=n; i++)
+bool prime[1000000];
+void prime_fill(int n){
+	prime[0] = true;
+	prime[1] = true;
+	for (__int64 i = 2; i <= n; i++)
 		if (!prime[i])
-			for (__int64 j=i*i; j<=r; j+=i)
+			for (__int64 j = i+i; j <= n; j += i)
 				prime[j] = true;
+}
 
 /*
 Hash-функция с несколькими модулями
 */
-__int64 AmountOfMods=3;
-__int64 mods[5]={1000000007,1000000009,524287,107,101};
-__int64 p=129;
-struct hash
+__int64 AmountOfMods = 3;
+__int64 mods[5] = { 1000000007, 1000000009, 524287, 107, 101 };
+__int64 pri = 129;
+struct hsh
 {
 	__int64 Eq_Mod[5];
 };
-void StringToHash(string s,hash *ans){
-	for(__int64 i=0;i<(__int64)s.length();i++){
-		__int64 chr=s[i];
-		for(__int64 mod=0;mod<AmountOfMods;mod++){
-			ans[i].Eq_Mod[mod]=ans[max(i-1,0)].Eq_Mod[mod];
-			ans[i].Eq_Mod[mod]+=chr;
-			ans[i].Eq_Mod[mod]%=mods[mod];
+void StringToHash(string s, hsh *ans){
+	for (__int64 i = 0; i<(__int64)s.length(); i++){
+		__int64 chr = s[i];
+		for (__int64 mod = 0; mod<AmountOfMods; mod++){
+			ans[i].Eq_Mod[mod] = ans[max(i - 1, 0LL)].Eq_Mod[mod] * pri;
+			ans[i].Eq_Mod[mod] += chr;
+			ans[i].Eq_Mod[mod] %= mods[mod];
 		}
 	}
 }
-bool hash_equals(hash a,hash b){
-	for(__int64 mod=0;mod<AmountOfMods;mod++)
-		if(a.Eq_Mod[mod]!=a.Eq_Mod[mod])
-			return false;
+bool equals(hsh a, hsh b){
+	for (__int64 mod = 0; mod<AmountOfMods; mod++)
+	if (a.Eq_Mod[mod] != a.Eq_Mod[mod])
+		return false;
 	return true;
 }
 
 /*
-Дейкстра с priority_queue 
+Дейкстра с priority_queue
 O(NlogN)
 */
-__int64 Dijxtra(__int64 countOfElements,__int64 start,__int64 finish,vector<pair<__int64,__int64> > *g){
+__int64 Dijxtra(__int64 countOfElements, __int64 start, __int64 finish, vector<pair<__int64, __int64> > *g){
 	vector<__int64>d;
-	d.resize(countOfElements,INF);
+	d.resize((unsigned int)countOfElements, INF);
 	vector<bool>used;
-	used.resize(countOfElements);
-	priority_queue<pair<__int64,__int64> >q;
-	d[start]=0;
-	q.push(make_pair(0,start));
-	while(!q.empty()){
-		__int64 v=q.top().second;
+	used.resize((unsigned int)countOfElements);
+	priority_queue<pair<__int64, __int64> >q;
+	d[(unsigned int)start] = 0;
+	q.push(make_pair(0, start));
+	while (!q.empty()){
+		__int64 v = q.top().second;
 		q.pop();
-		if(used[v])
+		if (used[(unsigned int)v])
 			continue;
-		if(d[v]==INF)
+		if (d[(unsigned int)v] == INF)
 			break;
-		used[v]=true;
-		if(v==finish){
-			return d[v];
+		used[(unsigned int)v] = true;
+		if (v == finish){
+			return d[(unsigned int)v];
 		}
-		for(__int64 i=0;i<g[v].size();i++){
-			__int64 to=g[v][i].first;
-			if(d[to] > d[v] + g[v][i].second){
-				d[to]=d[v] + g[v][i].second;
-				q.push(make_pair(-d[to],to));
+		for (__int64 i = 0; i<g[v].size(); i++){
+			__int64 to = g[(unsigned int)v][(unsigned int)i].first;
+			if (d[(unsigned int)to] > d[(unsigned int)v] + g[(unsigned int)v][(unsigned int)i].second){
+				d[(unsigned int)to] = d[(unsigned int)v] + g[(unsigned int)v][(unsigned int)i].second;
+				q.push(make_pair(-d[(unsigned int)to], to));
 			}
 		}
 	}
@@ -200,79 +179,81 @@ __int64 Dijxtra(__int64 countOfElements,__int64 start,__int64 finish,vector<pair
 /*
 Геометрия
 */
-struct po__int64{
+struct point{
 	double x, y;
-	void r(){
-		cin >> x >> y;
-	}
-	void write(){
-		cout << x << " " << y << endl;
-	}
-	po__int64(){}
-	po__int64(__int64 x, __int64 y) :x(x), y(y){}
+	point(){}
+	point(double x, double y) :x(x), y(y){}
 };
-double operator + (po__int64 a, po__int64 b){
+double operator + (point a, point b){
 	return sqrt((a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y));
 }
-__int64 operator * (po__int64 a, po__int64 b){
+double operator * (point a, point b){
 	return a.x*b.y - a.y*b.x;
 }
-double operator % (po__int64 a, po__int64 b){
+double operator % (point a, point b){
 	return a.x*b.x + a.y*b.y;
 }
-po__int64 operator - (po__int64 a, po__int64 b){
-	po__int64 ans;
+point operator - (point a, point b){
+	point ans;
 	ans.x = a.x - b.x;
 	ans.y = a.y - b.y;
 	return ans;
 }
-po__int64 root(0, 0);
-bool cmp(po__int64 a, po__int64 b){
-	if ((a - root)*(b - root) > 0)
+point rot(0, 0);
+bool cmp(point a, point b){
+	if ((a - rot)*(b - rot) > 0)
 		return true;
-	if ((a - root)*(b - root) < 0)
+	if ((a - rot)*(b - rot) < 0)
 		return false;
-	if (a + root > b + root)
+	if (a + rot > b + rot)
 		return true;
 	return false;
 }
-po__int64 p90(po__int64 a){
-	return po__int64(-a.y, a.x);
+point p90(point a){
+	return point(-a.y, a.x);
 }
-po__int64 p180(po__int64 a){
-	return po__int64(-a.x, -a.y);
+point p180(point a){
+	return point(-a.x, -a.y);
 }
-po__int64 p270(po__int64 a){
-	return po__int64(a.y, -a.x);
+point p270(point a){
+	return point(a.y, -a.x);
 }
 
 struct circle{
-	po__int64 o;
+	point o;
 	__int64 r;
 };
 
 /*
-Реализация стека
+Реализация стека с динамической длинной
 push(x) - добавляет элемент x
 pop() - удаляет элемент
-get_top() - возвращает верхний элемент 
+get_top() - возвращает верхний элемент
 */
-class My_Stack
-{
-private:
-	__int64 st[100000000];
-	__int64 top;
-public:
-	void push(__int64 x){
-		st[top++]=x;
+template<class type>
+struct Stack{
+	vector<type>st;
+	int top;
+	void push(type x){
+		if (top >= st.size())
+			st.push_back(type());
+		st[top++] = x;
 	}
 	void pop(){
-		top--;
+		top = (top == 0 ? top : top - 1);
 	}
-	__int64 get_top(){
-		return st[top-1];
+	type get_top(){
+		return (top == 0 ? 0 : st[top - 1]);
 	}
 };
+
+/*
+Example:
+Stack<int>a;
+a.pop();
+a.push();
+a.get_top();
+*/
 
 /*
 Дерево отрезков
@@ -290,7 +271,7 @@ private:
 	__int64 tree[1000000];
 	__int64 upd[1000000];
 public:
-	void build(__int64 v, __int64 l, __int64 r,__int64 *a){
+	void build(__int64 v, __int64 l, __int64 r, __int64 *a){
 		upd[v] = NONE;
 		if (r - l == 1){
 			tree[v] = a[l];
@@ -327,7 +308,7 @@ public:
 		modify(from, to, value, 2 * v + 2, m, r);
 		tree[v] = tree[2 * v + 1] + tree[2 * v + 2];
 	}
-	__int64 sum(__int64 from,__int64 to,__int64 v,__int64 l,__int64 r){
+	__int64 sum(__int64 from, __int64 to, __int64 v, __int64 l, __int64 r){
 		if (from <= l && r <= to){
 			return tree[v];
 		}
